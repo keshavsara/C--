a7ea96f01c294cea517f4af8899daccdfedbfd4f
keshavsara <119835044+keshavsara@users.noreply.github.com>
2024-08-13 17:26:40 +0530

    pointer

--- Pointers/Arrayinpointer.cpp
+++ Pointers/Arrayinpointer.cpp
@@ -13,5 +13,11 @@ int main()
     int arr[]={10,20,30};
     cout<<*arr<<endl;
     
-
+    int *ptr = arr;
+    for(int i =0;i<3;i++)
+    {
+        cout<<*ptr<<endl;
+        ptr++;
+    }
+    return 0;
 }
